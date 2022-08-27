25332c8c1a2d0ae1d01a5c56ebfdd11fda1077a7
Sohlar <drew@robosim.com>
2022-08-27 16:20:16 -0700

    Update test.py

--- env/test.py
+++ env/test.py
@@ -1,3 +1,5 @@
+from pydoc import pager
+from tracemalloc import start
 from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.common.by import By
@@ -12,12 +14,18 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com/")
 time.sleep(3)
 currentElem = driver.switch_to.active_element
-#print("Value of CSS: " + currentElem.value_of_css_property())
-test = currentElem.get_attribute('href')
-#print(currentElem.get_dom_attribute('id'))
-#print("property: " + currentElem.get_property)
-#print("aria_role: " + currentElem.aria_role)
-#print("acessible_name: " + currentElem.accessible_name)
-#print("location: " + currentElem.location)
-#print("parent: " + currentElem.parent)
+print("Value of CSS: " + currentElem.value_of_css_property())
+
+print("aria_role: " + currentElem.aria_role)
+print("acessible_name: " + currentElem.accessible_name)
+print("location: " + currentElem.location)
+print("parent: " + currentElem.parent)
 driver.find_element(By.id, currentElem.id).send_keys("Please god work")
+
+start
+    load page
+        click element
+            add to script
+                y
+                    execute JS (active_element) -> xpath function -> return Xpath to python
+                n
